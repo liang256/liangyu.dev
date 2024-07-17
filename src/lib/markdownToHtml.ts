92af669a04b3fc0ede840b1c2ee4db55d5e7ba86
Liang Chen <liangyu.chen.dev@gmail.com>
2024-07-17 13:51:58 -0400

    refactor: Add rehype-picture plugin for image optimization

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -5,10 +5,15 @@ import rehypeFormat from 'rehype-format'
 import rehypeStringify from 'rehype-stringify'
 import rehypeRaw from 'rehype-raw'
 import rehypePrism from 'rehype-prism-plus' // Add this import for Prism.js
+import rehypePicture from 'rehype-picture'
 
 export default async function markdownToHtml(content: string) {
   const processedContent = await unified()
     .use(remarkParse)
+    .use(rehypePicture, {
+      jpg: {webp: 'image/webp'},
+      png: {svg: 'image/svg+xml'}
+    })
     .use(remarkRehype, { allowDangerousHtml: true })
     .use(rehypeRaw)
     .use(rehypePrism) // Add rehypePrism for syntax highlighting
