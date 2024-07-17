93321abe7ee965f7a3970717f0badcb92c95ac35
Liang Chen <liangyu.chen.dev@gmail.com>
2024-07-17 12:34:16 -0400

    feat: post list page

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,25 @@
-import { NavBar } from "@/app/_components/nav-bar";
 import { getSortedPostsData } from "@/services/posts";
+import { PostPreview } from "../_components/post-preview";
 
 export default function PostList() {
+  const posts = getSortedPostsData();
   return (
     <div>
-      <h1>Posts</h1>
-      <p>This is the Posts page.</p> 
+      <h1 className="text-3xl">Posts</h1>
+      <div className="mt-4">
+      {posts.map((post) => (
+        // <p>{JSON.stringify(post)}</p>
+        <PostPreview
+          key={post.id}
+          title={post.title}
+          coverImage={post.coverImage}
+          date={post.date}
+          author={post.author}
+          slug={post.id}
+          excerpt={post.excerpt}
+        />
+      ))}
+      </div>
     </div>
   );
 }
