965f7e013c630c36a58f6a17913a2b68f4f279a4
Liang Chen <liangyu.chen.dev@gmail.com>
2024-07-18 15:36:11 -0400

    refactor: Update navigation bar component layout and styling

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -3,25 +3,25 @@ import Link from "next/link";
 export function NavBar() {
   return (
     <div className="my-10 flex justify-center space-x-4">
-    <nav>
-      <ul className="flex justify-center space-x-4">
-        <li>
-          <Link href="/">
-            <span>Projects</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/posts">
-            <span>Blog</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <span>About</span>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+      <nav>
+        <ul className="flex justify-center space-x-4">
+          <li>
+            <Link href="/">
+              <span>Projects</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/posts">
+              <span>Blog</span>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <span>About</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
